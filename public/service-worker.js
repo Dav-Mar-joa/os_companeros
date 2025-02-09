bd02227aaedb4bfcdfef7ff7f1385a138a842c65
David MARTINS <david.joaquim.martins@gmail.com>
2025-02-09 01:41:04 +0100

    2 COMPTE NON ADMIS + LIKE

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,29 @@
+const CACHE_NAME = 'os-companeros-v1';  // Utilise un nom de cache unique pour chaque version
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/assets/css/styles.css',
-          '/assets/icons/icon192.png',
-          '/assets/icons/icon512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/css/styles.css',
+        '/assets/icons/icon192.png',  // Vérifie que l'icône existe à cet emplacement
+        '/assets/icons/icon512.png'   // Vérifie que l'icône existe à cet emplacement
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(fetchedResponse) {
+        // Cache la nouvelle ressource pour les requêtes suivantes
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, fetchedResponse.clone());
+        });
+        return fetchedResponse;
+      });
+    })
+  );
+});
