0b8b529922dd0a68a8a2b5e7f6d9564959a43f86
David MARTINS <david.joaquim.martins@gmail.com>
2025-02-09 01:06:48 +0100

    2 COMPTE NON ADMIS + LIKE

--- app.js
+++ app.js
@@ -88,6 +88,9 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/profil', express.static(path.join(__dirname, 'public')));
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'service-worker.js'));
+});
 
 // Servir les fichiers statiques
 app.use(express.static(path.join(__dirname, 'public')));
